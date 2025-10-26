d04aa3562647bbc5e25df7410f97e6a4ea03e489
Martijn van Ginkel <martijnvginkel@gmail.com>
2025-10-26 12:12:34 +0000

    added some logging to tests

--- src/codec/codec-ping.adb
+++ src/codec/codec-ping.adb
@@ -1,5 +1,8 @@
 with Ada.Streams; use  Ada.Streams;
 with Interfaces; use  Interfaces;
+with Ada.Text_IO; use  Ada.Text_IO;
+with Ada.Integer_Text_IO; use  Ada.Integer_Text_IO;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 package body Codec.Ping is
 
@@ -14,11 +17,13 @@ package body Codec.Ping is
       I     : Stream_Element_Offset := 1;
    begin
       -- Encode header (little endian)
+      Put_Line ("[Encode] Encoding header with Msg_Type:" & Integer'Image (Integer (Msg.Hdr.Msg_Type)) & " (2 bytes)");
       Bytes (I)     := Stream_Element (Msg.Hdr.Msg_Type and 16#FF#);
       Bytes (I + 1) := Stream_Element (Shift_Right (Msg.Hdr.Msg_Type, 8));
       I := I + 2;
 
       -- Encode payload (ASCII)
+      Put_Line ("[Encode] Encoding payload with content: '" & Msg.Payload & "' (4 bytes)");
       for C of Msg.Payload loop
          Bytes (I) := Stream_Element (Character'Pos (C));
          I := I + 1;
@@ -45,12 +50,14 @@ package body Codec.Ping is
          Msg.Hdr.Msg_Type := Low or High;
       end;
       I := I + 2;
+      Put_Line ("[Decode] Decoded header with Msg_Type:" & Integer'Image (Integer (Msg.Hdr.Msg_Type)) & " (2 bytes)");
 
       -- Decode payload (4 ASCII bytes)
       for J in Msg.Payload'Range loop
          Msg.Payload (J) := Character'Val (Integer (Data (I)));
          I := I + 1;
       end loop;
+      Put_Line ("[Decode] Decoded payload with content: '" & Msg.Payload & "' (4 bytes)");
 
       return Msg;
    end Decode;
